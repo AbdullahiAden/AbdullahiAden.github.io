df66cf766de0baed48cd519303fadfce310abb55
Abdullahi Aden <Abbe6152@gmail.com>
2020-11-04 18:16:42 +0100

    first commit in new

---
+++ app/js/Portfolio.js
@@ -0,0 +1,30 @@
+// var workText = document.querySelector(".workText");
+// workText.getCon
+
+
+// selcting the strong element, developer 
+var strong = document.querySelector("strong");
+
+
+
+// setting boolean to evaluate it to false first
+var dev = false;
+
+
+setInterval(function(){
+    // if the text is developer, change to designer  
+    if (dev){
+        strong.textContent="Develop";
+    }else{
+        strong.textContent="Design"
+        strong.style.color="#34A1B9"
+    } 
+
+    dev=!dev; // here we are changing our boolean evaluator to contradict the last decision, 
+
+},1000)
+
+
+
+
+
