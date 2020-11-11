bfb3a8fec009787068c92985b68374e54b599ad2
Abdullahi Aden <Abbe6152@gmail.com>
2020-11-11 20:38:53 +0100

    responsive work

--- app/js/Portfolio.js
+++ app/js/Portfolio.js
@@ -1,10 +1,25 @@
 
-let view = document.getElementById('view');
 
-view.addEventListener("click", ()=>{
+var mainNavbar = document.querySelector("#mainNavbar");
+function toggleShow() {
+    mainNavbar.style.visibility = "visible"
+    mainNavbar.style.opacity = "1"
+};
+function toggleClose() {
+    mainNavbar.style.visibility = "hidden"
+    mainNavbar.style.opacity = "0"
+};
 
-    console.log("hyyy");
-})
+toggleClose()
+toggleShow()
+
+console.log('hh');
+// let view = document.getElementById('view');
+
+// view.addEventListener("click", ()=>{
+
+//     console.log("hyyy");
+// })
 
 // var registerAccountButton = document.getElementById('registerAccountButton');
 // var registerAccountModal = new bootstrap.Modal(document.getElementById('registerAccountModal'), {
